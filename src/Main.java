0193eb22dc4f101b5bd73b4e347c0ff6f9fffd5f
albarelloshernan <albarelloshernan@users.noreply.github.com>
2022-06-15 12:04:59 -0300

    Concurrency with multiple threads.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Thread t = new Thread(new ThreadedProcess());
-        t.start();
+        try{
+            ThreadedProcess tp = new ThreadedProcess();
+            Thread t1 = new Thread(tp, "Thread_1");
+            Thread t2 = new Thread(tp, "Thread_2");
+            Thread t3 = new Thread(tp, "Thread_3");
+            Thread t4 = new Thread(tp, "Thread_4");
+            Thread t5 = new Thread(tp, "Thread_5");
+            t1.start();
+            t2.start();
+            t3.start();
+            t4.start();
+            t5.start();
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+            t5.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted!");
+        }
     }
 }
