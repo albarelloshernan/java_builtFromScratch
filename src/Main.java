ab415c0113b543f884ac65de824defe3e6f48f8a
albarelloshernan <albarelloshernan@users.noreply.github.com>
2022-06-15 12:35:36 -0300

    Concurrency with multiple identified threads in sequence.

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,20 @@ public class Main {
             Thread t3 = new Thread(tp, "Thread_3");
             Thread t4 = new Thread(tp, "Thread_4");
             Thread t5 = new Thread(tp, "Thread_5");
+            tp.threadId = t1.getName();
             t1.start();
-            t2.start();
-            t3.start();
-            t4.start();
-            t5.start();
             t1.join();
+            tp.threadId = t2.getName();
+            t2.start();
             t2.join();
+            tp.threadId = t3.getName();
+            t3.start();
             t3.join();
+            tp.threadId = t4.getName();
+            t4.start();
             t4.join();
+            tp.threadId = t5.getName();
+            t5.start();
             t5.join();
         } catch (InterruptedException e) {
             System.out.println("Interrupted!");
--- src/ThreadedProcess.java
+++ src/ThreadedProcess.java
@@ -1,6 +1,7 @@
 public class ThreadedProcess implements Runnable{
+    public String threadId;
     @Override
     public void run() {
-        System.out.println("Hello world in thread!");
+        System.out.println("Hello world in thread named " + threadId + "!");
     }
 }
