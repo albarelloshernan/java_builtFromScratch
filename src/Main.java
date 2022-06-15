f52b54c891890c432edb586944ea937f26e5ae4b
albarelloshernan <albarelloshernan@users.noreply.github.com>
2022-06-15 14:24:42 -0300

    Global counter updated by multiple threads with syncronization.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
             Thread t1 = new Thread(tp, "Thread_1");
             Thread t2 = new Thread(tp, "Thread_2");
             t1.start();
-            t1.join();
             t2.start();
+            t1.join();
             t2.join();
             System.out.println(tp.counter);
         } catch (InterruptedException e) {
-            System.out.println("Interrupted!");
+            System.out.println("Interrupted in Main!");
         }
     }
 }
--- src/ThreadedProcess.java
+++ src/ThreadedProcess.java
@@ -1,10 +1,16 @@
 public class ThreadedProcess implements Runnable{
     public int counter;
-    private final int rounds = 100000;
     @Override
     public void run() {
-        for(int i = 0; i < rounds; i++) {
-            counter++;
+        final int rounds = 10;
+        int id;
+        String name = Thread.currentThread().getName();
+        id = name.equals("Thread_1") ? 1 : 2;
+        for (int i = 0; i < rounds; i++) {
+            synchronized (this) {
+                counter++;
+                System.out.println("ThreadId: " + id);
+            }
         }
     }
 }
