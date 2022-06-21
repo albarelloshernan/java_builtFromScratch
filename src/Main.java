4867c5fa642698186ba54fd6c65539c18c303d6d
albarellos.h.a@outlook.com <albarellos.h.a@outlook.com>
2022-06-21 14:07:18 -0300

    Simple thread pool.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
-        ThreadedProcess t1, t2;
-        t1 = new ThreadedProcess("Thread-1");
-        t2 = new ThreadedProcess("Thread-2");
-        t1.t.start();
-        t2.t.start();
-        try{
-            t1.t.join();
-            t2.t.join();
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted in Main!");
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for(int i = 0; i < 10; i++){
+            Runnable worker = new WorkerThread("" + i);
+            executor.execute(worker);
         }
+        executor.shutdown();
+        while (!executor.isTerminated()){}
+        System.out.println("Finished all threads.");
     }
 }
